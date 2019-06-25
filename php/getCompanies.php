a94cae401dd675375e339bd32151b322034cc525
Adam Jonsson <adamsmajl@gmail.com>
2019-06-25 21:33:36 +0200

    Create database model for safer and better queries
    The current system is using mysqli witch works but you need alot of code
    to make a query safe for injection

--- php/getCompanies.php
+++ php/getCompanies.php
@@ -1,22 +1,8 @@
 <?php
-    include_once('config.php');
-    $connection = dbConnect($hostname, $username, $password, $database);
-    $query = "SELECT * FROM companies19;";
-    $result = queryDB($connection, $query);
-
-    $companies = array();
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_object()) {
-            $company;
-            $company['id'] = $row->id;
-            $company['name'] = $row->name;
-            $company['description'] = $row->description;
-            $company['website'] = $row->website;
-            $company['logo'] = $row->logo;
-            $company['main_sponsor'] = $row->main_sponsor;
-            array_push($companies, $company);
-        }
-    }
-    echo json_encode($companies);
-    $connection->close();
-?>
+    include_once('database.php');
+    $dabaseModel = new DatabaseModel();
+    $dabaseModel->establishDatabaseConnection();
+    $result = $dabaseModel->dbSelectAllSimple(
+        'SELECT * FROM companies19', PDO::FETCH_ASSOC
+    );
+    echo json_encode($result);
