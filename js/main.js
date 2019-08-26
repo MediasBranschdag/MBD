95d6ce8004573f95cb66f73e03eff9b4ac184a28
= <adamsmajl@gmail.com>
2019-08-26 07:59:18 +0200

    improve hideme function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.hideme').each( function(i){
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top + 100;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_object ){
