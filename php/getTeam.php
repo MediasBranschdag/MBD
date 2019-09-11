4c8ceca6d0716c7fcccf9b7f5f6d0230b30b0b6d
= <adamsmajl@gmail.com>
2019-09-11 20:25:36 +0200

    Add new get team model

--- php/getTeam.php
+++ php/getTeam.php
@@ -1,24 +1,76 @@
 <?php
-    include_once('config.php');
-    $connection = dbConnect($hostname, $username, $password, $database);
-    $query = "SELECT * FROM team20 ORDER BY position, name";
-    $result = queryDB($connection, $query);
 
-    $team = array();
-    sort($team);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_object()) {
-            $teamMember;
-            $teamMember['name'] = $row->name;
-            $teamMember['position'] = $row->position;
-            $teamMember['email'] = $row->email;
-            $teamMember['image'] = $row->image;
-            $teamMember['linkedin'] = $row->linkedin;
-            array_push($team, $teamMember);
+    include_once('database.php');
+
+    class TeamModel extends DatabaseModel {
+        const SELECT_ATTRIBUTES = "*";
+
+        public function __construct() {
+            $this->establishDatabaseConnection();
         }
-    } else {
-        echo "0 results";
+
+        /**
+         * Getting all the team members
+         */
+        public function getAllTeamMembers() {
+            return $this->dbSelectAllSimple(
+                'SELECT 
+                    ' . TeamModel::SELECT_ATTRIBUTES . '
+                FROM 
+                    team20 
+                ORDER BY
+                    CASE position
+                        WHEN "Företagssamordnare" THEN 1
+                        WHEN "Företagsansvarig" THEN 2
+                        WHEN "Projektledare" THEN 3
+                        ELSE 4
+                    END,
+                    position
+                    ASC'
+            );
+        }
+
+        /**
+         * Getting only the team mebers that is responsable of all the companies
+         */
+        public function getCompanyResponsible() {
+            return $this->dbSelectAllSimple(
+                'SELECT 
+                    ' . TeamModel::SELECT_ATTRIBUTES . '
+                FROM 
+                    team20 
+                WHERE
+                    position = "Företagsansvarig" OR
+                    position = "Företagssamordnare"
+                ORDER BY
+                    position, name'
+            );
+        }
     }
-    echo json_encode($team);
-    $connection->close();
+
+    class TeamController {
+
+        public function init() {
+            //Create model
+            $teamModel = new TeamModel();
+
+            //Check request
+            switch ($_GET['action']) {
+                case 'company-responsible':
+                    $data = $teamModel->getCompanyResponsible();
+                break;
+                
+                default:
+                    $data = $teamModel->getAllTeamMembers();
+                    break;
+            }
+
+            //Convert the data to json
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        }
+    }
+
+    $controller = new TeamController();
+    $controller->init();
 ?>
