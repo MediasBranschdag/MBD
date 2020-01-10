d39c0ff64ab428a0483b9af9e714f4dd62446c28
= <adamsmajl@gmail.com>
2020-01-10 08:20:16 +0100

    Added a cookie model

--- js/model.js
+++ js/model.js
@@ -1,4 +1,4 @@
-MBDApp.factory("TranslationModel", function($http, $window) {
+MBDApp.factory("TranslationModel", function($http, $window, CookieModel) {
 
   var phrases = {};
   this.getPhrases = function() {
@@ -12,19 +12,8 @@ MBDApp.factory("TranslationModel", function($http, $window) {
     setLanguageFromCookie(response.data);
   });
 
-  function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-  }
-
   function setLanguageFromCookie(translations) {
-    var lang = readCookie('lang');
+    var lang = CookieModel.readCookie('lang');
     if (lang == null) {
       lang = 'se'
     }
@@ -57,7 +46,7 @@ MBDApp.factory("TranslationModel", function($http, $window) {
   }
 
   this.getActiveLanguage = function() {
-    var lang = readCookie('lang');
+    var lang = CookieModel.readCookie('lang');
     if (lang == null) {
       lang = 'se'
     }
@@ -67,7 +56,6 @@ MBDApp.factory("TranslationModel", function($http, $window) {
   this.setLanugage = function(lang) {
     document.cookie = "lang= " + lang;
     $window.location.reload();
-    location.re
   }
 
   return this;
