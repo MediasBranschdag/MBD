e573b9682db3f9741906ade14ac503a344ebe75e
= <adamsmajl@gmail.com>
2020-02-06 20:03:40 +0100

    Init react project

--- js/profileCtrl.js
+++
@@ -1,5 +0,0 @@
-MBDApp.controller('ProfileCtrl', function($scope, TranslationModel) {
-    $scope.getPosition = function(person) {
-        return TranslationModel.choosePhrase(person.position_se, person.position_en);
-    }
-});
--- php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOST', 'MBD-mysql');
-	define('DB_NAME', '166397-mediasbranschdag');
-	define('DB_USERNAME', 'user');
-	define('DB_PASSWORD', 'root');
-?>
