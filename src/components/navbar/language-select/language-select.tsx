d2949d83b43367bfd02a1cd01c32e881014e6b6a
= <adamsmajl@gmail.com>
2020-02-10 21:56:12 +0100

    Make language select bigger and fixed warning

--- src/components/navbar/language-select/language-select.tsx
+++ src/components/navbar/language-select/language-select.tsx
@@ -7,7 +7,7 @@ import TranslationModel from '../../../model/translationModel';
 
 const LanguageSelect: FC = (props) => {
     return (
-        <div onClick={() => TranslationModel.setLanguage(TranslationModel.getLanguage() == 'se' ? 'en' : 'se')} className='language-select'>
+        <div onClick={() => TranslationModel.setLanguage(TranslationModel.getLanguage() === 'se' ? 'en' : 'se')} className='language-select'>
             <img className="language-select-globe" src={globeIcon} alt="Globe"/>
             <div className="language-select-text">
                 {TranslationModel.getLanguage()}
