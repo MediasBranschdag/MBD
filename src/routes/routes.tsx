32376a33aefb3248471f94287b08e461b127b904
= <adamsmajl@gmail.com>
2020-04-04 21:39:57 +0200

    Add student page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,7 @@ import Companypage from '../pages/company-page/companypage';
 import phrases from '../data/translations.json';
 import { Phrase } from '../model/translationModel';
 import Contactpage from '../pages/contact-page/contact-page';
+import Studentpage from '../pages/student-page/studentpage';
 
 class Route {
     path: string;
@@ -20,7 +21,7 @@ class Route {
 const Routes = {
     homePage: new Route('/', phrases.start, Homepage),
     compnayPage: new Route('/company', phrases.for_companies, Companypage),
-    studentPage: new Route('/student', phrases.for_students, Companypage),
+    studentPage: new Route('/student', phrases.for_students, Studentpage),
     mapPage: new Route('/map', phrases.map, Companypage),
     contactPage: new Route('/contact', phrases.contact, Contactpage),
     eventPage: new Route('/event', phrases.events, Companypage),
