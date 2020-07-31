cc54427098831d50f6dd242853bc2d574649c58a
ellaklara <ellaklara.westerlund@gmail.com>
2020-07-31 17:05:02 +0200

    Add Dinner Party Admin

--- public/backend/getDinnerParty.php
+++ public/backend/getDinnerParty.php
@@ -29,6 +29,160 @@
                 'SELECT * FROM dinner_party_courses WHERE year = ' . $currentYear
             );
         }
+        
+        public function getCourseStats() {
+            $currentYear = $this->exhibitDateModel->getCurrentYear();
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json');
+            
+            return $this->dbSelectAllSimple(
+                'SELECT 
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en,
+                COUNT(*) AS quantity
+            FROM
+                dinner_party_guests guests INNER JOIN
+                dinner_party_courses course ON guests.starterId = course.id
+            GROUP BY
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en
+            UNION
+            SELECT 
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en,
+                COUNT(*) AS quantity
+            FROM
+                dinner_party_guests guests INNER JOIN
+                dinner_party_courses course ON guests.mainCourseId = course.id
+            GROUP BY
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en
+            UNION
+            SELECT 
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en,
+                COUNT(*) AS quantity
+            FROM
+                dinner_party_guests guests INNER JOIN
+                dinner_party_courses course ON guests.dessertId = course.id
+            GROUP BY
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en
+            UNION
+            SELECT 
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en,
+                COUNT(*) AS quantity
+            FROM
+                dinner_party_guests guests INNER JOIN
+                dinner_party_courses course ON guests.drinksId = course.id
+            GROUP BY
+                course.id,
+                course.year,
+                course.type,
+                course.desc_se,
+                course.desc_en,
+                course.attributes_se,
+                course.attributes_en'
+            );
+        }
+
+        public function getAllergies() {
+            $currentYear = $this->exhibitDateModel->getCurrentYear();
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json');
+            
+            return $this->dbSelectAllSimple(
+                'SELECT DISTINCT allergies
+                FROM dinner_party_guests guests 
+                WHERE guests.allergies IS NOT NULL AND
+                guests.allergies <> ""'
+            );
+        }
+
+        public function getGuestStats() {
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json');
+            
+            return $this->dbSelectAllSimple(
+                'SELECT guests.type, COUNT(*) as quantity FROM dinner_party_guests guests GROUP BY guests.type'
+            );
+        }
+
+        public function getCompanyStats() {
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json');
+            
+            return $this->dbSelectAllSimple(
+                'SELECT companies.name, COUNT(*) as quantity FROM dinner_party_guests guests JOIN companies ON guests.companyId = companies.id GROUP BY companies.name'
+            );
+        }
+
+        public function getGuests() {
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json');
+            
+            return $this->dbSelectAllSimple(
+                'SELECT 
+                    guests.id,
+                    guests.name,
+                    guests.personId,
+                    guests.email,
+                    guests.type,
+                    companies.name,
+                    starter.desc_se AS starter_se,
+                    starter.desc_en AS starter_en,
+                    mainCourse.desc_se AS mainCourse_se,
+                    mainCourse.desc_en AS mainCourse_en,
+                    dessert.desc_se AS dessert_se,
+                    dessert.desc_en AS dessert_en,
+                    drinks.desc_se AS drinks_se,
+                    drinks.desc_en AS drinks_en,
+                    guests.allergies,
+                    guests.ticketPrice
+                FROM dinner_party_guests guests
+                LEFT JOIN companies ON guests.companyId = companies.id
+                JOIN dinner_party_courses starter ON guests.starterId = starter.id
+                JOIN dinner_party_courses mainCourse ON guests.mainCourseId = mainCourse.id
+                JOIN dinner_party_courses dessert ON guests.dessertId = dessert.id
+                JOIN dinner_party_courses drinks ON guests.drinksId = drinks.id'
+            );
+        }
 
         /**
          * Getting all the events
@@ -69,8 +223,23 @@
                 case 'add-guest':
                     $data = $eventModel->addGuest();
                     break;
+                case 'get-guests':
+                    $data = $eventModel->getGuests();
+                    break;
                 case 'get-courses':
                     $data = $eventModel->getCourses();
+                    break;
+                case 'get-course-stats':
+                    $data = $eventModel->getCourseStats();
+                    break;
+                case 'get-guest-stats':
+                    $data = $eventModel->getGuestStats();
+                    break;
+                case 'get-company-stats':
+                    $data = $eventModel->getCompanyStats();
+                    break;
+                case 'get-allergies':
+                    $data = $eventModel->getAllergies();
                     break;
                 default:
                     $data = $eventModel->getDinnerParty();
--- src/model/dinnerPartyModel.tsx
+++ src/model/dinnerPartyModel.tsx
@@ -1,6 +1,7 @@
 import BACKEND_PATH from '../backend-environment'
 import axios from 'axios'
 import { Phrase } from './translationModel'
+import phrases from '../data/translations.json'
 
 export interface DinnerParty {
     year: number
@@ -9,6 +10,22 @@ export interface DinnerParty {
     ticketBasePrice: number
     alcoholPrice: number
     helperRebate: number
+    googleSheetsId: string
+}
+
+
+export enum GuestType {
+    Student = 'student',
+    CompanyRep = 'companyRep',
+    Helper = 'helper',
+    PlusOne = 'plusOne',
+}
+
+export const guestPhrases = {
+    [GuestType.Student]: phrases.dinner_admin.students,
+    [GuestType.CompanyRep]: phrases.dinner_admin.company_reps,
+    [GuestType.Helper]: phrases.dinner_admin.helpers,
+    [GuestType.PlusOne]: phrases.dinner_admin.plus_ones,
 }
 
 export enum CourseType {
@@ -20,10 +37,27 @@ export enum CourseType {
 }
 
 export interface Course {
-    id: number,
+    id: number
     type: CourseType
     desc: Phrase
     attributes: Phrase
+}
+
+export type CourseQuanitity = Course & { quantity: number }
+
+export interface Guest {
+    id: number
+    name: string
+    personId: string
+    email: string
+    type: GuestType
+    company: string
+    starter: Phrase
+    mainCourse: Phrase
+    dessert: Phrase
+    drinks: Phrase
+    allergies: string
+    ticketPrice: number
 }
 
 function parseDinnerParty(dinnerParty: DinnerParty): DinnerParty {
@@ -60,6 +94,31 @@ function parseCourse(courseJson: any): Course {
     }
 }
 
+function parseGuest(guestJson: any): Course {
+    return {
+        ...guestJson,
+        /*
+        ...{
+            starter: {
+                se: guestJson.starter_se,
+                en: guestJson.starter_en,
+            },
+            mainCourse: {
+                se: guestJson.mainCourse_se,
+                en: guestJson.mainCourse_en,
+            },
+            dessert: {
+                se: guestJson.dessert_se,
+                en: guestJson.dessert_en,
+            },
+            drinks: {
+                se: guestJson.drinks_se,
+                en: guestJson.drinks_en,
+            },
+        },*/
+    }
+}
+
 export async function getDinnerParty(): Promise<DinnerParty> {
     const response = await axios.get(BACKEND_PATH + 'getDinnerParty.php')
     return parseDinnerParty(response.data)
@@ -71,4 +130,62 @@ export async function getCourses(): Promise<Course[]> {
     )
     const data = response.data.map((course: any) => parseCourse(course))
     return data
+}
+
+export async function getGuests(): Promise<Guest[]> {
+    const response = await axios.get(
+        BACKEND_PATH + 'getDinnerParty.php?action=get-guests'
+    )
+    const data = response.data.map((guest: any) => parseGuest(guest))
+    return data
+}
+
+export async function getGuestStats(): Promise<any[]> {
+    const response = await axios.get(
+        BACKEND_PATH + 'getDinnerParty.php?action=get-guest-stats'
+    )
+    return response.data
+}
+
+export async function getCourseStats(): Promise<CourseQuanitity[]> {
+    const response = await axios.get(
+        BACKEND_PATH + 'getDinnerParty.php?action=get-course-stats'
+    )
+    const data = response.data.map((course: any) => {
+        return { ...parseCourse(course), ...{ quantity: course.quantity } }
+    })
+    return data
+}
+
+export async function getCompanyStats(): Promise<{ name: string, quantity: number}> {
+    const response = await axios.get(
+        BACKEND_PATH + 'getDinnerParty.php?action=get-company-stats'
+    )
+    return response.data
+}
+
+export async function getAllergies(): Promise<string[]> {
+    const response = await axios.get(
+        BACKEND_PATH + 'getDinnerParty.php?action=get-allergies'
+    )
+    const data = response.data.map((data: { allergies: string}) => data.allergies)
+    return data
+}
+
+export async function updateGoogleSheet(accessToken: string): Promise<boolean> {
+    const dinnerParty = await getDinnerParty()
+    const guests = await getGuests()
+    const guestValues = guests.map((guest) => Object.values(guest))
+    axios.put(
+        `https://sheets.googleapis.com/v4/spreadsheets/${dinnerParty.googleSheetsId}/values/A2/?key=AIzaSyATibyeiHShJwmqQWhVKc6fRg_98cfcKtc&valueInputOption=RAW`,
+        {
+            values: guestValues,
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
+    )
+    return true
 }
