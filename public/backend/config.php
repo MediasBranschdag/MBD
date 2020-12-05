bc1fd0e4b39777cf5465ddf42fe8518874f25e6a
ellaklara <ellaklara.westerlund@gmail.com>
2020-12-06 00:14:27 +0100

    Update backend

--- public/backend/config.php
+++ public/backend/config.php
@@ -1,11 +1,4 @@
 <?php
-
-	// For production
-	// define('DB_HOST', 'mediasbranschdag-166397.mysql.binero.se');
-	// define('DB_NAME', '166397-mediasbranschdag');
-	// define('DB_USERNAME', '166397_lt74885');
-	// define('DB_PASSWORD', 'Cxj75RZk56');
-
 	// For local development
 	define('DB_HOST', 'MBD-mysql');
 	define('DB_NAME', '166397-mediasbranschdag');
