be2641b7e6e0a518d7d40442a0f2a214e2d4de8e
Johanna Nilsen <Jnilse@kth.se>
2022-04-04 09:23:38 -0400

    Adding companies in navbar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -8,7 +8,7 @@ import Studentpage from '../pages/student-page/student-page';
 //import Dinnerpage from '../pages/dinner-page/dinnerpage';
 import DinnerAdmin from '../pages/dinner-admin/dinner-admin';
 import WeArePage from '../pages/we-are-page/we-are-page';
-//import MapPage from '../pages/map-page/map-page';
+import MapPage from '../pages/map-page/map-page';
 import Eventpage from '../pages/event-page/event-page';
 import Temppage from '../pages/under-construction-page/temp-page';
 
@@ -32,10 +32,11 @@ const Routes = {
     homePage: new Route('/', phrases.start, Homepage),
     whatIsPage: new Route('/medieteknik', phrases.we_are_media_technology.about_media_technology, WeArePage),
     compnayPage: new Route('/company', phrases.for_companies, Companypage),
-    //studentPage: new Route('/exhibitors', phrases.for_students, Studentpage),
+    studentPage: new Route('/exhibitors', phrases.exhibitors, Studentpage),
+    //mapPage: new Route('/map', phrases.map, MapPage),
+
     contactPage: new Route('/contact', phrases.contact, Contactpage),
 
-    //mapPage: new Route('/map', phrases.map, MapPage),
     //dinnerPartyPage: new Route('/dinner', phrases.dinner_party, Dinnerpage),
     //dinnerAdminPage: new Route('/dinner-admin', phrases.dinner_admin, DinnerAdmin, false),
     //eventPage: new Route('/event', phrases.events, Companypage),
