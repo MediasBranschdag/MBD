5e1b81e62aae393c4a81af11cd3b0df06b2d0f20
Johanna Nilsen <Jnilse@kth.se>
2022-04-04 09:22:47 -0400

    Chaning Eyevinn card to black need to change back

--- src/components/company-logo-list/company-logo-list.tsx
+++ src/components/company-logo-list/company-logo-list.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 import './company-logo-list.css';
 import { Company } from '../../model/companyModel';
 
-const CompanyLogoList: FC<{companies: Array<Company>}> = (props) => {
+const CompanyLogoList: FC<{ companies: Array<Company> }> = (props) => {
     return (
         <div className='company-logo-list'>
             {props.companies.map(company => {
                 return <a key={company.id} href={'http://' + company.url}>
-                    <div className="company-logo-item">
-                        <img src={"/assets/companies/" + company.logo_path} alt=""/>
+                    <div className={"company-logo-item"}>
+                        <img src={"/assets/companies/" + company.logo_path} alt="" />
                     </div>
                 </a>
             })}
